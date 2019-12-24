02bbd4329f98925357b940d3feb2f86584ebbb92
Ujjawal <ujjawaldixit099@gmail.com>
2019-12-24 12:31:24 +0530

    Add librfkafka producer

---
+++ KafkaProducer.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func produce() {
+
+	producer, errorIfProducerNotCreated := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "127.0.0.1:9092"})
+
+	if errorIfProducerNotCreated != nil {
+		fmt.Println("Could not create Kafka Producer", errorIfProducerNotCreated)
+	}
+
+	topic := "test"
+
+	deliveryChan := make(chan kafka.Event)
+
+	value := "From Kafkatail!!"
+
+	err := producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          []byte(value),
+		Headers:        []kafka.Header{{Key: "myTestHeader", Value: []byte("header values are binary")}},
+	}, deliveryChan)
+
+	if err != nil {
+		fmt.Println("Error in producing message to kafka", err)
+	}
+
+	e := <-deliveryChan
+	m := e.(*kafka.Message)
+
+	if m.TopicPartition.Error != nil {
+		fmt.Printf("Delivery failed: %v\n", m.TopicPartition.Error)
+	} else {
+		fmt.Printf("Delivered message to topic %s to partition %d at offset %v\n",
+			*m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
+	}
+
+	close(deliveryChan)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kafkatail
 
 go 1.12
+
+require github.com/confluentinc/confluent-kafka-go v1.3.0
