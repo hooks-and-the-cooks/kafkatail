e7ae4d93db0a457e632d5d55c4eae4bfc8c10cbf
Ujjawal <ujjawaldixit099@gmail.com>
2019-12-24 15:34:34 +0530

    Add basic cli inteface

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/kafkatail
 
 go 1.12
 
-require github.com/confluentinc/confluent-kafka-go v1.3.0
+require (
+	github.com/confluentinc/confluent-kafka-go v1.3.0
+	github.com/urfave/cli/v2 v2.0.0
+)
--- main.go
+++ main.go
@@ -2,11 +2,33 @@ package main
 
 import (
 	"fmt"
+	"github.com/urfave/cli/v2"
+	"os"
 )
 
 func main() {
-	fmt.Println("Lets start !!")
+	flags := []cli.Flag{
+		&cli.StringFlag{
+			Name:  "b",
+			Usage: "broker address",
+			Value: "127.0.0.1:9092",
+		}}
 
-	//produce()
-	consume()
+	app := cli.App{
+		Name:        "kafkatail",
+		Usage:       "Kafka CLI",
+		Version:     "0.1.0",
+		Description: "Kafka Consumer and Producer CLI in golang",
+		Copyright:   "Ujjawal Dixit",
+		Flags:       flags,
+		Action: func(context *cli.Context) error {
+			fmt.Println("Hello from cli!!")
+			return nil
+		}}
+
+	err := app.Run(os.Args)
+
+	if err != nil {
+		fmt.Println("Something wrong happened at startup!")
+	}
 }
